2b74f35f5842ea873dfd149dd79380f0147d4479
el18069 <el18069@mail.ntua.gr>
2021-12-20 19:59:57 +0200

    backend testing

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,12 @@ const express = require('express')
 // set up express app
 const app = express();
 
+app.get('/', function(req, res){
+    console.log('GET request');
+    res.send({"poutsa" : "3idi", "mouni" : "den3erw"});
+});
+
+
 //listen for requests
 app.listen(process.env.port || 4000, function(){
     console.log('Hello, World');
