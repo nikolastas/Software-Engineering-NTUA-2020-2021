75c626260c30f7325ae57f74970b71409a38864c
el18069 <el18069@mail.ntua.gr>
2021-12-20 20:45:14 +0200

    postman stage

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const routes = require('./routes/api');
+
 // set up express app
 const app = express();
 
-app.use(routes);
+//initialize routes
+app.use('/api', require('./routes/api'));
 
 //listen for requests
 app.listen(process.env.port || 4000, function(){
