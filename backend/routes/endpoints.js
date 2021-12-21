1a51a453c81d12b6db0df4657f3baac5fea0cdab
el18069 <el18069@mail.ntua.gr>
2021-12-21 04:04:06 +0200

    fixed SQL query and A and correctly getting results

--- backend/routes/endpoints.js
+++ backend/routes/endpoints.js
@@ -6,21 +6,20 @@ const moment = require('moment');
 
 
 router.get('/PassesPerStation/:stationID/:date_from/:date_to', function(req, res){
-    // console.log(req.params.stationID);
-    // var datefrom = req.params.date_from;
-    // var dateto = req.params.date_to;
-    //console.log(date.slice(0,4)+'-'+date.slice(4,6)+'-'+date.slice(6,8));
-    // console.log(req.params.date_to);
+    var datefrom = req.params.date_from;
+    var dateto = req.params.date_to;
+    
     try{
+        console.log('entered');
+        var str = "SELECT * FROM passes, stations "+"WHERE stations.stationID = passes.StationsstationID AND passes.StationsstationID = '"+
+        req.params.stationID+"' AND passes.timestamp <= '"+ datefrom.slice(0,4) + "-"+ datefrom.slice(4,6)+"-"+
+        datefrom.slice(6,8)+" 00:00:00' AND passes.timestamp >= '"+dateto.slice(0,4)+"-"+dateto.slice(4,6)+"-"+dateto.slice(6,8) +"23:59:59'";
+        console.log(str);
         console.log("koukou");
-        con.query("SELECT * FROM passes, stations "+
-        `WHERE stations.stationID = passes.StationsstationID 
-        AND passes.StationsstationID = ${req.params.stationID} AND passes.timestamp <= 
-        '${datefrom.slice(0,4)+'-'+datefrom.slice(4,6)+'-'+datefrom.slice(6,8)} 00:00:00'`+
-        `AND passes.timestamp >= '${dateto.slice(0,4)+'-'+dateto.slice(4,6)+'-'+dateto.slice(6,8)} 23:59:59'` ,
+        con.query(str,
         function(err, result, fields){
             if (err) throw err
-            console.log(result[0]);
+            console.log(result);
         });
     }catch(err){
         //handle error
