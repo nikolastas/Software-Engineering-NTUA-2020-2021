92869438b6c5b29b5d4739916342a47945715f27
NicoleMp <el18604@mail.ntua.gr>
2022-01-17 02:33:44 +0200

    telos 32

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Home from './Home';
 import { BrowserRouter as Router , Route, Switch } from 'react-router-dom';
 import Create from './Create';
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
   const title = "SoftEng";
@@ -22,7 +23,10 @@ function App() {
             </Route>
             <Route path="/blogs/:id">
               <BlogDetails />
-            </Route>        
+            </Route>    
+            <Route path="*">
+              <NotFound />
+            </Route>    
           </Switch>
           
         </div>
--- frontend/src/Create.js
+++ frontend/src/Create.js
@@ -1,9 +1,61 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
+
 const Create = () => {
-    return (
-      <div className="create">
-        <h2>Add a New Blog</h2>
-      </div>
-    );
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('mario');
+  const [isPending, setIsPending] = useState(false);
+  const history = useHistory();
+
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    fetch('http://localhost:8000/blogs/', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog)
+    }).then(() => {
+      setIsPending(false);
+      history.push('/');
+    })
+    setIsPending(true);
   }
-   
-  export default Create;
+
+  return (
+    <div className="create">
+      <h2>Add a New Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog title:</label>
+        <input 
+          type="text" 
+          required 
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Blog author:</label>
+        <select
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        >
+          <option value="mario">mario</option>
+          <option value="yoshi">yoshi</option>
+        </select>
+        { !isPending && <button>Add Blog</button>}
+        { isPending && <button disabled>Adding Blog...</button>}
+      </form>
+    </div>
+  );
+}
+ 
+export default Create;
