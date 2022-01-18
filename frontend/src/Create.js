539384f56949fd30895f301d9a6b4eb1ff3826d3
NicoleMp <el18604@mail.ntua.gr>
2022-01-18 16:46:29 +0200

    before 3rd merch

--- frontend/src/Create.js
+++ frontend/src/Create.js
@@ -58,29 +58,10 @@ const Create = () => {
             },
             body: formBody,
             signal: abortCont.signal
-        })
-            .then(res => {
-                if (!res.ok) { // error coming back from server
-                    throw Error('could not fetch the data for that resource');
-                }
-                return res.json();
-            })
-            .then(data => {
-                setIsPending(false);
-                setData(data);
-                setError(null);
-            })
-            .catch(err => {
-                if (err.name === 'AbortError') {
-                    console.log('fetch aborted')
-                } else {
-                    // auto catches network / connection error
-                    setIsPending(false);
-                    setError(err.message);
-                }
-            })
+        }
+        )
     }, 1000);
-   }
+}
 
   return (
     <div className="create">
