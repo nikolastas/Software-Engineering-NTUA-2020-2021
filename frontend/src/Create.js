14cb753a50af6a5c21843fef77db7bc6f7c0ee2a
NicoleMp <el18604@mail.ntua.gr>
2022-01-18 15:33:12 +0200

    ti fash

--- frontend/src/Create.js
+++ frontend/src/Create.js
@@ -1,30 +1,49 @@
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
+import usePostFetch from "./usePostFetch";
 
 
 const Create = () => {
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [type, setType] = useState('Admin');
-  const [isPending, setIsPending] = useState(false);
-  const history = useHistory();
-
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [type, setType] = useState('admin');
+    const [isPending, setIsPending] = useState(false);
+    const history = useHistory();
+    let error;
 
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const user = { username, password , type , email };
+    // const user = {};
+    // user.username = username;
+    // user.password = password;
+    // user.typeofuser = type;
+    // user.email = email;
+    // console.log(user);
+    // fetch('http://localhost:9103/interoperability/api/signup', {
+    //   method: 'POST',
+    //   headers: { "Content-Type": "application/json" },
+    //   body: JSON.stringify(user)
+    // }).then((e) => {
+    //     console.log(e);
+    //   setIsPending(false);
+    //   history.push('/');
+    // })
+    // setIsPending(true);
 
-    fetch('http://localhost:9103/interoperability/api/signup', {
-      method: 'POST',
-      //headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(user)
-    }).then(() => {
-      setIsPending(false);
-      history.push('/');
-    })
-    setIsPending(true);
+    let details = {
+        'username': username,
+        'password': password,
+        'typeOfUser': type,
+        'email' : email
+    };
+    console.log(details);
+  // const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
+  // 
+   const {user , error1 , isP} = usePostFetch('http://localhost:9103/interoperability/api/signup', details);
+    setIsPending(isP);
+    error = error1;
    }
 
   return (
@@ -56,11 +75,12 @@ const Create = () => {
           value={type}
           onChange={(e) => setType(e.target.value)}
         >
-          <option value="Admin">Admin</option>
-          <option value="User">User</option>
+          <option value="admin">admin</option>
+          <option value="user">user</option>
         </select>
         { !isPending && <button>Εγγραφή</button>}
         { isPending && <button disabled>Αναμονή...</button>}
+        { error && <div>{ error }</div> }
       </form>
     </div>
   );
---
+++ frontend/src/usePostFetch.js
@@ -0,0 +1,59 @@
+import { useState, useEffect } from 'react';
+
+const usePostFetch = (url, details) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const abortCont = new AbortController();
+
+    let formBody = [];
+    for (let property in details) {
+      let encodedKey = encodeURIComponent(property);
+      let encodedValue = encodeURIComponent(details[property]);
+      formBody.push(encodedKey + "=" + encodedValue);
+    }
+    formBody = formBody.join("&");
+    
+   
+
+
+    setTimeout(() => {
+        fetch('http://localhost:9103/interoperability/api/signup', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+            },
+            body: formBody
+          })
+      .then(res => {
+        if (!res.ok) { // error coming back from server
+          throw Error('could not fetch the data for that resource');
+        } 
+        return res.json();
+      })
+      .then(data => {
+        setIsPending(false);
+        setData(data);
+        setError(null);
+      })
+      .catch(err => {
+        if (err.name === 'AbortError') {
+          console.log('fetch aborted')
+        } else {
+          // auto catches network / connection error
+          setIsPending(false);
+          setError(err.message);
+        }
+      })
+    }, 1000);
+
+    // abort the fetch
+    return () => abortCont.abort();
+  }, [url, details])
+
+  return { data, isPending, error };
+}
+ 
+export default usePostFetch;
