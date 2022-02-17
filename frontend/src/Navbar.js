b623f2811bdc9bb0f3e7b30ef3ea423f0d5a377f
el18069 <el18069@mail.ntua.gr>
2022-02-17 20:28:28 +0200

    cookies fault

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,6 +1,11 @@
 import { Link } from 'react-router-dom'
+import { LoginContext } from "./Context/LoginContext";
+import { useContext } from "react";
 
 const Navbar = () => {
+    const {globalUsername, setGlobalUsername,
+        globalLoginToken, setGlobalLoginToken} = useContext(LoginContext);
+   
     return (  
         <nav className="navbar">
             <h1>E-Pass opou pas</h1>
@@ -10,6 +15,10 @@ const Navbar = () => {
                 <Link to="/create">Εγγραφή</Link>
                 <Link to="/data">Διελεύσεις</Link>
                 <Link to="/logout">Log out</Link>
+                
+            </div>
+            <div>
+            {globalUsername && <h3>Hello {globalUsername}</h3>}
             </div>
         </nav>
     );
--- frontend/src/useFetch.js
+++ frontend/src/useFetch.js
@@ -1,15 +1,23 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
+import { LoginContext } from "./Context/LoginContext";
 
 const useFetch = (url) => {
   const [data, setData] = useState(null);
   const [isPending, setIsPending] = useState(true);
   const [error, setError] = useState(null);
-
+  const {globalUsername, setGlobalUsername,
+    globalLoginToken, setGlobalLoginToken} = useContext(LoginContext);
+  
   useEffect(() => {
     const abortCont = new AbortController();
-
+    console.log('Cookie should be' , document.cookie);
     setTimeout(() => {
-      fetch(url, { signal: abortCont.signal })
+      fetch(url, {
+        signal: abortCont.signal,
+        headers:{
+          "x-observatory-auth": globalLoginToken
+        }
+        })
       .then(res => {
         if (!res.ok) { // error coming back from server
           throw Error('could not fetch the data for that resource');
